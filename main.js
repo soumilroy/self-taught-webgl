f74bf0194e5c27e7a9efe25cb79e6141f8ac4e0c
Soumil Roy <soumil.roy@empowerreg.ai>
2025-11-03 23:18:59 +0530

    mind fucky

--- main.js
+++ main.js
@@ -16,16 +16,25 @@ camera.position.set(0, 0, 2);
  */
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("#canvas") });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
 
 /**
- * Object
+ * Object(s)
 */
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 scene.add(cube);
-renderer.render(scene, camera);
 
+const sphereGeometry = new THREE.SphereGeometry(1, 12, 12);
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.4, 16, 64);
+const torusMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+const torus = new THREE.Mesh(torusGeometry, torusMaterial);
+scene.add(torus);
 /**
  * Animation
  */
@@ -33,6 +42,9 @@ function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.005;
   cube.rotation.y += 0.005;
+  sphere.rotation.y += 0.005;
+  torus.rotation.x += 0.005;
+  torus.rotation.y += 0.005;
   renderer.render(scene, camera);
 }
 animate();
